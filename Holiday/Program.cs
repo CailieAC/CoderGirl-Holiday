4a5d7e58f1cadcd862f6907d939294de19773127
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-10 22:49:33 -0500

    commit1

--- Holiday/Program.cs
+++ Holiday/Program.cs
@@ -6,9 +6,23 @@ namespace Holiday
     {
         public static void Main()
         {
+            Console.Write("Enter date you are leaving on vacation: ");
+            DateTime dayLeaving = DateTime.Parse(Console.ReadLine());
+            Console.Write("How many days will you be gone: ");
+            int daysGone = int.Parse(Console.ReadLine());
+
+            DayOfWeek dayOfReturn = DayReturning(dayLeaving, daysGone);
+
+            Console.WriteLine(dayOfReturn);
             Console.ReadLine();
         }
 
         // TODO: Create a method that takes the day you are leaving on vacation and how many days you will be gone and return the name of the day of the week you will return.
+        public static DayOfWeek DayReturning(DateTime dayLeaving, int daysGone)
+        {
+            DayOfWeek dayOfReturn;
+            dayOfReturn = dayLeaving.AddDays(daysGone).DayOfWeek;
+            return dayOfReturn;
+        }
     }
 }
